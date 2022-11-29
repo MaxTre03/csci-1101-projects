fb0d79f6d7d8caf042ae3c2cff409859e1fb309d
Maximiliano <Maximiliano.trevino@outlook.com>
2022-11-29 10:55:00 -0600

    Add project files

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO  sandbox(int64_value, double_value, string_value) VALUES(77, 2.69, 'why'); 
+
+UPDATE sandbox SET double_value = 78.4343 WHERE id = 124;
+
+--DELETE FROM sandbox WHERE id = 94;
+
+SELECT * FROM sandbox;
